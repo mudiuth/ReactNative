2b5f5dafaded53f43fd7cf12215db9e6b0850a04
uthman semu <uthman@uth.uth.local>
2025-06-15 08:01:23 +0300

    added the fst session quiz

---
+++ ses1.js
@@ -0,0 +1,12 @@
+console.log('my name is Uthman Semu')
+/*what is the syntax to add an external Javascript file?
+	<script type="javascript" src="file_path_for_the_.js_file"></script>
+can you run JavaScript in a file with a JS extension in your browser?
+	yes
+How do you write a multiple-line comment in javaScript?
+	by using the '/*content to be commented*/
+/*What is the best way to remove a line of code from running that you might
+want to keep as you debug?
+	by commenting it out using the // operations
+	*/
+
