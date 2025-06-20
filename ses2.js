0ab0fa0eea02e8ca2b96a07a99c2307072bd9925
uthman semu <uthman@uth.uth.local>
2025-06-20 18:48:34 +0300

    added some changes

---
+++ ses2.js
@@ -0,0 +1,7 @@
+let num1 = prompt('enter the fst number')
+num1 = parseInt(num1)
+
+let num2 = prompt("enter the 2nd number ")
+num2 = parseInt(num2)
+
+comsole.log(""+num1+"+"+num2+" =", num1+num2)
